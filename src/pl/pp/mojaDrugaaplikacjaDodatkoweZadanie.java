2f3ae2e132732eea6af437e6dfb179d0ef37745f
Artem <harbaar4381_aehit@students.vizja.pl>
2024-04-16 17:18:55 +0200

    Pierwszy zapis do repozytoriumLab2

---
+++ src/pl/pp/mojaDrugaaplikacjaDodatkoweZadanie.java
@@ -0,0 +1,21 @@
+package pl.pp;
+
+public class mojaDrugaaplikacjaDodatkoweZadanie {
+    public static void main(String[] args) {
+        // Tworzenie zmiennej całkowitej x i przypisanie wartości 10
+        int x = 10;
+
+        // Obliczanie dwukrotności liczby x oraz x^2
+        int dwukrotnosc_x = x * 2;
+        int x_kwadrat = x * x;
+
+        // Wyświetlanie wyników
+        System.out.println("x = " + x);
+        System.out.println("Dwukrotność liczby x = " + dwukrotnosc_x);
+        System.out.println("x^2 = " + x_kwadrat);
+
+
+    }
+}
+
+
